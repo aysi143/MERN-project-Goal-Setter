f20bef71b490a6f55b4fc238232861c3b8c163f0
AYSI <aysanewyonasj@gmail.com>
2022-02-18 19:51:06 +0300

    Initital REST api for

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,17 @@
+const Goal =require('../models/goalModel')
+
 const asyncHandler =require('express-async-handler')
 
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
 
-const getGoals = asyncHandler(async (req, res)=>{res.status(200).json({message:"get Goals"})})
+const getGoals = asyncHandler(async (req, res)=>{
+    const goals =await Goal.find()
+    res.status(200).json(goals)
+
+    // res.status(200).json({message:"get Goals"})
+    })
 
 // @desc Get goals
 // @route POST /api/goals
@@ -15,7 +22,11 @@ const setGoal = asyncHandler(async(req, res)=>{
      res.status(400)
      throw new Error('Please add a text field')
     }
-    
+    const goal = await Goal.create(
+        {
+            text: req.body.text,
+        }
+    )
     res.status(200).json({message: 'Set All names'})})
 
 // @desc PUT goals
@@ -23,14 +34,38 @@ const setGoal = asyncHandler(async(req, res)=>{
 // @access Private
 
 const updateGoal = asyncHandler(async(req, res)=>{
+    const goal =await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+    })
+    
     res.status(200).json(
-        {message: `Update goal ${req.params.id}`})})
+        updatedGoal
+       // {message: `Update goal ${req.params.id}`}
+        )
+    })
 
 // @desc DELETE goals
 // @route DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async(req, res)=>{
+    const goal =await Goal.findById(req.params.id)
+    if (!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    await goal.remove
     res.status(200).json(
-        {message:"Delete Goals"})})
+        {id:req.params.id})
+    })
 
-module.exports ={getGoals, setGoal, updateGoal, deleteGoal,}
+module.exports ={
+    getGoals,
+    setGoal, 
+    updateGoal,
+    deleteGoal,}
